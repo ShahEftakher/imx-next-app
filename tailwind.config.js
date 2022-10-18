9de2885928fd63d0dfa3045771e58844347f5962
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-10-18 17:17:21 +0600

    Added tailwindcss and configuration

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsz,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
