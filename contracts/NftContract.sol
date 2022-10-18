259e605a6a76ce8cfb236eefb0e4f335cfab69fd
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-10-18 15:43:38 +0600

    Added mintable contract for imx

--- contracts/NftContract.sol
+++ contracts/NftContract.sol
@@ -2,7 +2,21 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-contract NftContract is ERC721{
-    constructor() ERC721("My Collection", "MC"){
+import "./imx-mintable/Mintable.sol";
+
+contract NftContract is ERC721, Mintable {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _owner,
+        address _imx
+    ) ERC721(_name, _symbol) Mintable(_owner, _imx) {}
+
+    function _mintFor(
+        address user,
+        uint256 id,
+        bytes memory
+    ) internal override {
+        _safeMint(user, id);
     }
 }
