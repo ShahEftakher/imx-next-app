65d826784116079f2577131ab5816bbe60ce4f14
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-10-31 18:42:16 +0600

    Controller fucntion for components

--- src/components/CreateCollection.tsx
+++ src/components/CreateCollection.tsx
@@ -1,23 +1,95 @@
-import { CreateCollectionParams } from '@imtbl/imx-sdk';
+import { CreateCollectionParams, ImmutableXClient, sign } from '@imtbl/imx-sdk';
 import React, { Dispatch, SetStateAction, useState } from 'react';
+import { getSigner } from '../helper/getSigner';
 
-const CreateCollection = () => {
+interface CreateCollectionProps {
+  adminClient: ImmutableXClient | undefined;
+}
+
+const CreateCollection = ({ adminClient }: CreateCollectionProps) => {
   const [collectionParams, setCollectionParams] =
     useState<CreateCollectionParams>(Object);
-  const createCollection = () => {};
+
+  const createCollection = async () => {
+    if (!adminClient) {
+      return;
+    }
+    const signer = await getSigner();
+    const signerAddress = await signer.getAddress();
+    setCollectionParams((preState) => ({
+      ...preState,
+      owner_public_key: signerAddress,
+    }));
+    if (
+      !collectionParams.contract_address ||
+      !collectionParams.name ||
+      !collectionParams.project_id ||
+      !collectionParams.owner_public_key
+    ) {
+      return;
+    }
+    try {
+      const res = await adminClient.createCollection(collectionParams);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-      <div className="flex justify-between items-center mt-4">
+      <div className="flex flex-col justify-center items-center mt-4">
         <div>
-          <label>Contract Name: </label>
+          <label>Collection Name: </label>
           <input
             className="border-2 border-cyan-400 rounded"
             type="text"
             value={collectionParams.name}
             onChange={(event) => {
-              setCollectionParams((preState: any) => ({
+              setCollectionParams((preState) => ({
                 ...preState,
                 name: event.target.value,
+              }));
+            }}
+          />
+        </div>
+        <div>
+          <label>Contract address: </label>
+          <input
+            className="border-2 border-cyan-400 rounded"
+            type="text"
+            value={collectionParams.contract_address}
+            onChange={(event) => {
+              setCollectionParams((preState: any) => ({
+                ...preState,
+                contract_address: event.target.value,
+              }));
+            }}
+          />
+        </div>
+        <div>
+          <label>Metadata API URL: </label>
+          <input
+            className="border-2 border-cyan-400 rounded"
+            type="text"
+            value={collectionParams.metadata_api_url}
+            onChange={(event) => {
+              setCollectionParams((preState: any) => ({
+                ...preState,
+                metadata_api_url: event.target.value,
+              }));
+            }}
+          />
+        </div>
+        <div>
+          <label>Collection Display Image: </label>
+          <input
+            className="border-2 border-cyan-400 rounded"
+            type="text"
+            value={collectionParams.collection_image_url}
+            onChange={(event) => {
+              setCollectionParams((preState: any) => ({
+                ...preState,
+                collection_image_url: event.target.value,
               }));
             }}
           />
@@ -36,11 +108,13 @@ const CreateCollection = () => {
             }}
           />
         </div>
+      </div>
+      <div className="flex justify-center mt-3">
         <button
           className="border-4 p-2 bg-cyan-600 rounded-lg"
           onClick={createCollection}
         >
-          Deploy Contract
+          Create Collection
         </button>
       </div>
     </div>
--- src/components/CreateProject.tsx
+++ src/components/CreateProject.tsx
@@ -1,73 +1,86 @@
-import { CreateProjectParams } from '@imtbl/imx-sdk';
+import { CreateProjectParams, ImmutableXClient } from '@imtbl/imx-sdk';
 import React, { Dispatch, SetStateAction, useState } from 'react';
 
 interface CreateProjectProps {
-  projectParams: CreateProjectParams;
-  setProjectParams: Dispatch<
-    SetStateAction<{
-      name: string;
-      company_name: string;
-      contact_email: string;
-    }>
-  >;
+  adminClient: ImmutableXClient | undefined;
 }
 
-const CreateProject = () => {
+const CreateProject = ({ adminClient }: CreateProjectProps) => {
   const [projectParams, setProjectParams] =
     useState<CreateProjectParams>(Object);
-  const createproject = () => {};
+
+  const createproject = async () => {
+    if (
+      !projectParams.company_name ||
+      !projectParams.contact_email ||
+      !projectParams.name
+    ) {
+      return;
+    }
+    if (adminClient === undefined) {
+      return;
+    }
+    try {
+      const res = await adminClient.createProject(projectParams);
+      console.log(res);
+      projectParams.company_name = '';
+      projectParams.contact_email = '';
+      projectParams.name = '';
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
-      <div className="flex justify-between items-center mt-4">
-        <div>
-          <label>Name: </label>
-          <input
-            className="border-2 border-cyan-400 rounded"
-            type="text"
-            value={projectParams.name}
-            onChange={(event) => {
-              setProjectParams((preState) => ({
-                ...preState,
-                name: event.target.value,
-              }));
-            }}
-          />
-        </div>
-        <div>
-          <label>Email: </label>
-          <input
-            className="border-2 border-cyan-400 rounded"
-            type="text"
-            value={projectParams.contact_email}
-            onChange={(event) => {
-              setProjectParams((preState) => ({
-                ...preState,
-                contact_email: event.target.value,
-              }));
-            }}
-          />
-        </div>
-        <div>
-          <label>Company Name: </label>
-          <input
-            className="border-2 border-cyan-400 rounded"
-            type="text"
-            value={projectParams.company_name}
-            onChange={(event) => {
-              setProjectParams((preState: any) => ({
-                ...preState,
-                company_name: event.target.value,
-              }));
-            }}
-          />
-        </div>
-        <button
-          className="border-4 p-2 bg-cyan-600 rounded-lg"
-          onClick={createproject}
-        >
-          Create Project
-        </button>
+    <div className="flex flex-col justify-center items-center">
+      <div>
+        <label>Name: </label>
+        <input
+          className="border-2 border-cyan-400 rounded"
+          type="text"
+          value={projectParams.name}
+          onChange={(event) => {
+            setProjectParams((preState) => ({
+              ...preState,
+              name: event.target.value,
+            }));
+          }}
+        />
       </div>
+      <div>
+        <label>Email: </label>
+        <input
+          className="border-2 border-cyan-400 rounded"
+          type="text"
+          value={projectParams.contact_email}
+          onChange={(event) => {
+            setProjectParams((preState) => ({
+              ...preState,
+              contact_email: event.target.value,
+            }));
+          }}
+        />
+      </div>
+      <div>
+        <label>Company Name: </label>
+        <input
+          className="border-2 border-cyan-400 rounded"
+          type="text"
+          value={projectParams.company_name}
+          onChange={(event) => {
+            setProjectParams((preState: any) => ({
+              ...preState,
+              company_name: event.target.value,
+            }));
+          }}
+        />
+      </div>
+      <button
+        className="border-4 p-2 bg-cyan-600 rounded-lg"
+        onClick={createproject}
+      >
+        Create Project
+      </button>
     </div>
   );
 };
