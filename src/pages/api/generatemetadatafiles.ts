8ff0bbed31a36dfade2e16976c599bc618b72362
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-11-08 19:00:49 +0600

    metadata generation working

--- src/pages/api/generatemetadatafiles.ts
+++ src/pages/api/generatemetadatafiles.ts
@@ -1,22 +1,30 @@
 import fs from 'fs';
 import os from 'os';
+const rimraf = require('rimraf');
 
 const hanlder = async (req: any, res: any) => {
   const { tokenNumber, startingID, cid, desc } = req.body;
   const filePath = `${os.homedir()}/Documents`;
   const apiUrl = 'https://nftstorage.link/ipfs/' + cid;
-  console.log(cid);
-  for (let index = startingID; index <= tokenNumber; index++) {
+  console.log(apiUrl);
+
+  if (fs.existsSync(`${filePath}/metadata`)) {
+    rimraf.sync(`${filePath}/metadata`);
+  }
+  fs.mkdirSync(`${filePath}/metadata`);
+
+  for (let index = startingID; index <= tokenNumber + startingID - 1; index++) {
     const jsonData = `{
           "name": ${index},
           "description": ${desc},
           "image_url": ${apiUrl}/${index}.png,
         };`;
-    if (!fs.existsSync(filePath)) {
-      fs.rmSync(`${filePath}/metadata`);
-      fs.mkdirSync(`${filePath}/metadata`);
+    console.log(index);
+    try {
+      fs.writeFileSync(`${filePath}/metadata/${index}`, jsonData);
+    } catch (error) {
+      console.log(error);
     }
-    fs.writeFileSync(`${filePath}/metadata${index}`, jsonData);
   }
 
   res
