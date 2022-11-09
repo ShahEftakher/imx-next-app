6ad634f55a623f6dddfcf4c009885d8ee621c560
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-11-09 12:22:35 +0600

    Added new state to context

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -5,10 +5,18 @@ export const StateContext = createContext<any>(Object);
 const StateContextProvider = (props: any) => {
   const [deployedAddress, setDeployedAddress] = useState<string>('');
   const [ipfsDirCID, setIpfsDirCID] = useState<string>('');
+  const [metadataURL, setMetadataURL] = useState<string>('');
 
   return (
     <StateContext.Provider
-      value={{ deployedAddress, setDeployedAddress, ipfsDirCID, setIpfsDirCID }}
+      value={{
+        deployedAddress,
+        setDeployedAddress,
+        ipfsDirCID,
+        setIpfsDirCID,
+        metadataURL,
+        setMetadataURL,
+      }}
     >
       {props.children}
     </StateContext.Provider>
